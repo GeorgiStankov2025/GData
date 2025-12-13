9a7be1a27064a85232929c8d1abf2a3169101cbe
GeorgiStankov2025 <georgistankov05@gmail.com>
2025-12-13 18:22:04 +0200

    AddGroupChatsMessagesExceptionsList

---
+++ Exceptions/GroupChatMessagesExceptionList.cs
@@ -0,0 +1,22 @@
+﻿using GData.Entity;
+
+namespace GData.Exceptions
+{
+    public class GroupChatMessagesExceptionList
+    {
+        public Task<GroupchatMessage>  CreateMessageAuthorDoesNotExist()
+        {
+
+            throw new ArgumentNullException("The message cannot be created because it's author does not exist or was deleted.");
+
+        }
+
+        public Task<GroupchatMessage> CreateMessageGroupChatDoesNotExist()
+        {
+
+            throw new ArgumentNullException("The message cannot be created because it's groupchat was deleted or does not exist.");
+
+        }
+
+    }
+}
